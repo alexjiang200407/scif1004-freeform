951ee9fac6b8098d211990f37cda79ad312ae37d
alex jiang <alexjiang201507@gmail.com>
2025-07-27 22:39:14 +1000

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,95 @@
+import './style.css'
+
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.fromTo(
+  ".scroll-fade-in",
+  { opacity: 0, y: 50 },
+  {
+    opacity: 1,
+    y: 0,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".scroll-fade-in",
+			end: "top 2%",
+			toggleActions: "play reverse play reverse",
+    }
+  }
+);
+
+// Right swipe
+document.querySelectorAll(".scroll-swipe-right").forEach(el => {
+  gsap.fromTo(
+    el,
+    { opacity: 0, x: 50 },
+    {
+      opacity: 1,
+      x: 0,
+      duration: 0.8,
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: el,
+				end: "top 2%",
+        toggleActions: "play reverse play reverse",
+      }
+    }
+  );
+});
+
+// Left swipe
+document.querySelectorAll(".scroll-swipe-left").forEach(el => {
+  gsap.fromTo(
+    el,
+    { opacity: 0, x: -50 },
+    {
+      opacity: 1,
+      x: 0,
+      duration: 0.8,
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: el,
+				end: "top -2%",
+        toggleActions: "play reverse play reverse",
+      }
+    }
+  );
+});
+
+// Top swipe
+document.querySelectorAll(".scroll-swipe-top").forEach(el => {
+  gsap.fromTo(
+    el,
+    { opacity: 0, y: -50 },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 0.8,
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: el,
+        toggleActions: "play reverse play reverse",
+      }
+    }
+  );
+});
+
+document.querySelectorAll(".scroll-swipe-bottom").forEach(el => {
+  gsap.fromTo(
+    el,
+    { opacity: 0, y: 50 },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 0.8,
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: el,
+				end: "top 2%",
+        toggleActions: "play reverse play reverse",
+      }
+    }
+  );
+});
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+	content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // or whatever your files are
+  ],
+  theme: {
+			colors: {
+				primary: {
+					DEFAULT: '#1D4ED8',
+					light: '#3B82F6',
+					dark: '#1E40AF',
+				},
+			},
+
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: '#1D4ED8',
+					light: '#3B82F6',
+					dark: '#1E40AF',
+				},
+			},
+		}
+  },
+  plugins: [],
+};
