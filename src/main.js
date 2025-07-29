d3facf9bd12cf21951594ac5056f5506a5e7481b
alex jiang <alexjiang201507@gmail.com>
2025-07-30 01:29:47 +1000

    even more stuff

--- src/main.js
+++ src/main.js
@@ -6,18 +6,32 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.fromTo(
-  ".scroll-fade-in",
-  { opacity: 0, y: 50 },
+  "#plato-quote-sticky",
+  { opacity: 0, x: -50 },
   {
     opacity: 1,
-    y: 0,
+		x: 0,
     ease: "none",
     scrollTrigger: {
-      trigger: ".scroll-fade-in",
+      trigger: "#plato-quote-sticky-container",
 			toggleActions: "play reverse play reverse",
     }
   }
 );
+
+document.querySelectorAll(".scroll-fade-in").forEach(el => 
+gsap.fromTo(
+  el,
+  { opacity: 0 },
+  {
+    opacity: 1,
+    ease: "none",
+    scrollTrigger: {
+      trigger: el,
+			toggleActions: "play reverse play reverse",
+    }
+  }
+));
 
 // Right swipe
 document.querySelectorAll(".scroll-swipe-right").forEach(el => {
@@ -29,6 +43,7 @@ document.querySelectorAll(".scroll-swipe-right").forEach(el => {
       x: 0,
       duration: 0.8,
       ease: "power3.out",
+			delay: 0.3,
       scrollTrigger: {
         trigger: el,
         toggleActions: "play reverse play reverse",
@@ -47,6 +62,7 @@ document.querySelectorAll(".scroll-swipe-left").forEach(el => {
       x: 0,
       duration: 0.8,
       ease: "power3.out",
+			delay: 0.3,
       scrollTrigger: {
         trigger: el,
         toggleActions: "play reverse play reverse",
@@ -65,6 +81,7 @@ document.querySelectorAll(".scroll-swipe-top").forEach(el => {
       y: 0,
       duration: 0.8,
       ease: "power3.out",
+			delay: 0.3,
       scrollTrigger: {
         trigger: el,
         toggleActions: "play reverse play reverse",
@@ -82,6 +99,7 @@ document.querySelectorAll(".scroll-swipe-bottom").forEach(el => {
       y: 0,
       duration: 0.8,
       ease: "power3.out",
+			delay: 0.3,
       scrollTrigger: {
         trigger: el,
         toggleActions: "play reverse play reverse",
